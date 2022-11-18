cfca44771559f71595d662f67b4dfd4e7c2bb807
Raul Alberto Moheno Zavaleta <ramz.162025@gmail.com>
2022-11-17 20:57:52 -0500

    Crear rutas de atajo para acceder a las vistas de oposicion

--- src/modules/oppositions/router.js
+++ src/modules/oppositions/router.js
@@ -4,47 +4,74 @@ import ManageOppositionsModule from './views/Oppositions/'
 import CreateOppositionView from './views/CreateOpposition/index.vue'
 import UpdateOppositionView from './views/UpdateOpposition/index.vue'
 
+const shortcutRoutes = [
+  {
+    path: 'oposiciones',
+    redirect: { name: 'manage-oppositions' },
+    meta: {
+      middleware: [authMiddleware]
+    }
+  },
+  {
+    path: 'oposiciones/crear',
+    redirect: { name: 'create-opposition' },
+    meta: {
+      middleware: [authMiddleware]
+    }
+  },
+  {
+    path: 'update/',
+    redirect: { name: 'manage-oppositions' },
+    meta: {
+      middleware: [authMiddleware]
+    }
+  }
+]
+
 const moduleRoute = [
   {
-    path: '/oppositions',
+    path: '/',
     component: Module,
     redirect: { name: 'manage-oppositions' },
     children: [
       {
-        path: 'list',
-        name: 'manage-oppositions',
-        component: ManageOppositionsModule,
-        meta: {
-          middleware: [authMiddleware]
-        }
-      },
-      {
-        path: 'create',
-        name: 'create-opposition',
-        component: CreateOppositionView,
-        meta: {
-          middleware: [authMiddleware]
-        }
-      },
-      {
-        path: 'update/:id',
-        name: 'update-opposition',
-        component: UpdateOppositionView,
-        meta: {
-          middleware: [authMiddleware]
-        }
-      },
-      {
-        path: 'update/',
-        name: 'update-opposition-without-id',
+        path: '/oppositions',
+        component: Module,
         redirect: { name: 'manage-oppositions' },
-        meta: {
-          middleware: [authMiddleware]
-        }
-      }
+        children: [
+          {
+            path: 'list',
+            name: 'manage-oppositions',
+            component: ManageOppositionsModule,
+            meta: {
+              middleware: [authMiddleware]
+            }
+          },
+          {
+            path: 'create',
+            name: 'create-opposition',
+            component: CreateOppositionView,
+            meta: {
+              middleware: [authMiddleware]
+            }
+          },
+          {
+            path: 'update/:id',
+            name: 'update-opposition',
+            component: UpdateOppositionView,
+            meta: {
+              middleware: [authMiddleware]
+            }
+          }
+        ]
+      },
+      ...shortcutRoutes
     ]
   }
+
 ]
+
+console.log(moduleRoute)
 
 export default (router) => {
   router.addRoutes(moduleRoute)
