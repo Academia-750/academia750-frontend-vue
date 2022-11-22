541aad171cb3b0094c761cdc22417d26563bba0f
Raul Alberto Moheno Zavaleta <ramz.162025@gmail.com>
2022-11-22 04:17:05 -0500

    Refactorizar ActionsMethods del modulo de Actualizar Perfil

--- src/modules/profile/views/UpdateProfile/UpdateProfileComponent.js
+++ src/modules/profile/views/UpdateProfile/UpdateProfileComponent.js
@@ -2,12 +2,14 @@ import components from './component/components'
 import data from './component/data'
 import methods from './component/methods'
 import computed from './component/computed'
+import ActionsMethods from './component/ActionsMethods'
 
 export default {
   mixins: [
     components,
     data,
     methods,
+    ActionsMethods,
     computed
   ],
   mounted () {
--- src/modules/profile/views/UpdateProfile/component/methods.js
+++ src/modules/profile/views/UpdateProfile/component/methods.js
@@ -41,52 +41,6 @@ export default {
 
       return true
     },
-    async updateProfileApi () {
-      try {
-        await this.UpdateProfileAction({
-          data: {
-            'first-name': this.form.firstName,
-            'last-name': this.form.lastName,
-            'phone': this.form.phone,
-            'email': this.form.email
-          },
-          config: {
-            params: {
-              include: 'roles,roles-permissions'
-            }
-          }
-        })
-
-        this.$swal.fire({
-          icon: 'success',
-          toast: true,
-          title: 'Tus datos han sido actualizados con éxito.',
-          timer: 3000
-        })
-
-        this.loadDataUserProfile()
-        this.$loadingApp.disabledLoadingProgressLinear()
-        this.loadingButtonUpdateProfile = false
-        this.disabledButtonUpdateProfile = false
-      } catch (error) {
-        console.log(error)
-        this.$loadingApp.disabledLoadingProgressLinear()
-        this.loadingButtonUpdateProfile = false
-        this.disabledButtonUpdateProfile = false
-        if (error.response === undefined) {
-          this.$swal.fire({
-            icon: 'error',
-            title: 'Ha ocurrido un problema en la aplicación. Reportelo e intente más tarde',
-            showConfirmButton: true,
-            confirmButtonText: '¡Entendido!',
-            timer: 7500
-          })
-        } else if (error.response?.status === 422) {
-          this.ResetForm()
-          this.handlingErrorValidation(error.response.data.errors)
-        }
-      }
-    },
     async handlingErrorValidation(errorResponse = {}) {
       await this.$refs['FormUpdateMyProfile']['setErrors'](errorResponse)
       this.loadingButtonUpdateProfile = true
@@ -111,65 +65,7 @@ export default {
       this.cache.data.profile.lastName = last_name
       this.cache.data.profile.phone = phone
       this.cache.data.profile.email = email
-    },
-    async unsubscribeSystem () {
-
-      this.$swal.fire({
-        // icon: "error",
-        width: '800px',
-        imageUrl: 'https://cdn-icons-png.flaticon.com/512/725/725099.png',
-        imageWidth: 100,
-        imageHeigth: 100,
-        title: 'Dar de Baja',
-        // eslint-disable-next-line quotes
-        html: "<p class='text-center'>Nos da mucha pena despedirnos de ti. <br>Esperamos que durante tu estancia en Academia 750 hayas podido aprender y conseguir ser un mejor opositor o que tu sueño se haya cumplido con nosotros. <br><b>¡Esperamos verte pronto de nuevo!</b></p>",
-        confirmButtonText: 'Continuar con el proceso de baja',
-        confirmButtonColor: '#007bff',
-        showCancelButton: true,
-        cancelButtonColor: '#dc3545',
-        cancelButtonText: 'Cancelar'
-      }).then((result) => {
-        /* Read more about isConfirmed, isDenied below */
-        if (result.isConfirmed) {
-          this.UnsubscribeSystemProcess()
-        }
-      })
-    },
-    async UnsubscribeSystemProcess () {
-      try {
-        this.$loadingApp.enableLoadingProgressLinear()
-        this.loadingButtonUnsubscribeSystem = true
-        this.disabledButtonUnsubscribeSystem = true
-
-        await this.UnsubscribeSystemAction()
-
-        this.$manageTokenAuth.$remove_token_auth()
-        this.$router.push({
-          name: 'login'
-        })
-        this.$loadingApp.disabledLoadingProgressLinear()
-        this.loadingButtonUnsubscribeSystem = false
-        this.disabledButtonUnsubscribeSystem = false
-        this.$swal.fire({
-          icon: 'success',
-          toast: true,
-          title: 'Adiós! Esperamos verte por aquí después',
-          showConfirmButton: false,
-          timer: 4000
-        })
-      } catch (error) {
-        this.$loadingApp.disabledLoadingProgressLinear()
-        this.loadingButtonUnsubscribeSystem = false
-        this.disabledButtonUnsubscribeSystem = false
-        this.$swal.fire({
-          icon: 'error',
-          toast: true,
-          title: 'Ha ocurrido un problema en la aplicación. Reportelo e intente más tarde',
-          showConfirmButton: true,
-          confirmButtonText: '¡Entendido!',
-          timer: 7500
-        })
-      }
     }
+
   }
 }
