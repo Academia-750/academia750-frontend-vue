00bd3739f809e5ec56cb738985b12f7ecbc5b480
Raul Alberto Moheno Zavaleta <ramz.162025@gmail.com>
2022-12-15 17:05:16 -0500

    Cambiar iconos al menu

--- src/configs/navigation.js
+++ src/configs/navigation.js
@@ -10,8 +10,8 @@ export default {
       key: '',
       can: '*',
       items: [
-        { icon: 'mdi-view-dashboard-outline', key: '', text: 'Perfil de usuario', to: { name: 'update-my-profile' }, can: '*' },
-        { icon: 'mdi-view-dashboard-outline', key: '', text: 'Cambiar mi foto', to: { name: 'update-image-account' }, can: '*' },
+        { icon: 'mdi-account-circle', key: '', text: 'Perfil de usuario', to: { name: 'update-my-profile' }, can: '*' },
+        { icon: 'mdi-camera', key: '', text: 'Cambiar mi foto', to: { name: 'update-image-account' }, can: '*' },
         { icon: 'mdi-lock', key: '', text: 'Cambiar contraseña', to: { name: 'change-password' }, can: '*' }
       ]
     },
