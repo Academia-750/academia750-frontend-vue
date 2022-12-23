bc8649e1d8a1d2c2a5cba2b3b1ab520a2ead6a36
Raul Alberto Moheno Zavaleta <ramz.162025@gmail.com>
2022-12-23 00:31:32 -0500

    Añadir boton de carga y debounce al buscador de preguntas del tema

--- src/modules/questionsTopic/views/questions/component/methods.js
+++ src/modules/questionsTopic/views/questions/component/methods.js
@@ -12,6 +12,20 @@ export default {
         this.metaData = response.data.meta
       })
     },
+    searchFieldWithDebounce(value) {
+      this.searchFieldExecuted(value)
+    },
+    loadDatatatable() {
+      this.getQuestions({
+        topic_id: this.$route.params.id,
+        config: {
+          params: this.buildQueryParamsRequest()
+        }
+      }).then((response) => {
+        this.topicData = response.data.meta.topic
+        this.metaData = response.data.meta
+      })
+    },
     deleteQuestionConfirm(item) {
       this.currentItemsSelectedForDelete = item
       this.$refs['dialogConfirmDeleteAction'].showDialog = true
