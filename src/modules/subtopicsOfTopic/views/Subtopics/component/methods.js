006b0fedbbb7ad9c56c156445342ffebebf7f32c
Raul Alberto Moheno Zavaleta <ramz.162025@gmail.com>
2022-12-23 00:01:49 -0500

    Añadir boton de carga y debounce al buscador de subtemas de un tema

--- src/modules/subtopicsOfTopic/views/Subtopics/component/methods.js
+++ src/modules/subtopicsOfTopic/views/Subtopics/component/methods.js
@@ -11,6 +11,19 @@ export default {
         this.topicData = response.data.meta.topic
       } )
     },
+    searchFieldWithDebounce(value) {
+      this.searchFieldExecuted(value)
+    },
+    loadDatatatable() {
+      this.getSubtopics({
+        topic_id: this.$route.params.id,
+        config: {
+          params: this.buildQueryParamsRequest()
+        }
+      }).then((response) => {
+        this.topicData = response.data.meta.topic
+      })
+    },
     deleteSubtopicConfirm (item) {
       this.currentItemsSelectedForDelete = item
       this.$refs['dialogConfirmDeleteAction'].showDialog = true
--- src/modules/subtopicsOfTopic/views/Subtopics/index.vue
+++ src/modules/subtopicsOfTopic/views/Subtopics/index.vue
@@ -61,10 +61,28 @@
                 params: { id: topicData.id }
               }"
             />
+            <v-btn
+              small
+              elevation="24"
+              color="teal darken-2"
+              class="white--text ml-1"
+              title="Cargar nuevamente"
+              @click="loadDatatatable"
+            >
+              <v-icon
+                dark
+                small
+                class="mx-1"
+              >
+                mdi-reload
+              </v-icon>
+              <span class="d-none d-sm-inline">Cargar</span>
+            </v-btn>
           </div>
         </v-toolbar>
         <resource-text-field-search
           ref="ResourceTextFieldSearch"
+          @emitSearchTextBinding="searchFieldWithDebounce"
           @emitSearchWord="searchFieldExecuted"
         />
       </template>
