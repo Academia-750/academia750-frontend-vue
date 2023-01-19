210876b9bce35375260a50d38bfdee537a748419
Raul Alberto Moheno Zavaleta <ramz.162025@gmail.com>
2023-01-19 01:12:29 -0500

    Implementar nueva validacion de contraseña segura

--- src/modules/profile/rules/MustBeAPasswordSecure.js
+++ src/modules/profile/rules/MustBeAPasswordSecure.js
@@ -1,8 +1,15 @@
 import { extend } from 'vee-validate'
 
 extend('ItMustBeAPasswordSecure', {
+
+  // before: $@$!%*?&#^)(
+  // !”#$%&’)(*+,-./:;<=>?@]\[^_`}{|~
+
   validate: (value) => {
-    const PATTERN_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#^)(])([A-Za-z\d$@$!%*?&#)(^]|[^ ]){8,15}$/
+    //const PATTERN_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!”#$%&’)(*+,-./:;<=>?@]\[^_`}{|~])([A-Za-z\d!”#$%&’)(*+,-./:;<=>?@]\[^_`}{|~]|[^ ]){8,15}$/
+    const PATTERN_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#^=_/<>)(])([A-Za-z\d$@$!%*?&#^=_/<>)(]|[^ ]){8,15}$/
+
+    //const PATTERN_REGEX = /^(?=.[A-Z])(?=.[a-z])(?=.\d)(?=.[@$!%#?&])[A-Za-z\d@$!%#?&]{8,15}$/
 
     return PATTERN_REGEX.test(value)
   },
