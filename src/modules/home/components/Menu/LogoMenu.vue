a3bb4666f2425482a2d5f48c9c59d1aac91bc9ef
unknown <Raul Alberto Moheno Zavaleta>
2023-02-02 15:34:34 -0500

    Resolver incidencia 168

--- src/modules/home/components/Menu/LogoMenu.vue
+++ src/modules/home/components/Menu/LogoMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <v-toolbar-title>
-    <v-img src="/images/academia750/logo.svg"></v-img>
+    <v-img src="/images/academia750/logo.svg" width="40" height="40"></v-img>
   </v-toolbar-title>
 </template>
 
