3a90b3fc8bd250bf50e646ca83b4e0433a10b83c
kayiganwa04 <kayiganwa04@gmail.com>
2023-08-28 10:35:28 +0200

    new changes

---
+++ src/modules/tags/components/tags-list-columns.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    position: 1,
+    text: 'Nombre',
+    align: 'start',
+    sortable: true,
+    value: 'name',
+    show: true
+  },
+  {
+    position: 2,
+    sortable: false,
+    text: 'Acciones',
+    align: 'center',
+    value: 'actions-resource',
+    show: true
+  }
+]
---
+++ src/modules/tags/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card-text>
+    <div ref="sectionTabsItemsGroupsByStatusAccount">
+      <v-card flat>
+        <v-card-text>
+          <TagsList />
+        </v-card-text>
+      </v-card>
+    </div>
+  </v-card-text>
+</template>
+
+<script>
+import notifications from '@/mixins/notifications'
+
+export default {
+  name: 'TagsView',
+  components: {
+    TagsList: () =>
+      import(
+        /* webpackChunkName: "TagsList" */ './components/tags-list.vue'
+      )
+  },
+  mixins: [notifications],
+  data() {
+    return {
+      reloadDatatableUsers: false
+    }
+  },
+  beforeCreate() {
+    this?.$hasRoleMiddleware('admin')
+  },
+  mounted() {
+    this.loadNotifications()
+  },
+  head: {
+    title: {
+      inner: 'Gestión de Categorías'
+    }
+  }
+}
+</script>
