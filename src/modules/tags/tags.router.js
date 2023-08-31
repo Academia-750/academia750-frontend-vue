729f7b71c669e92ffdd29c59b24b11d2f1a9dcfd
Eddy Kayiganwa <kayiganwa04@gmail.com>
2023-08-31 03:31:43 +0200

    DBT-808 Feat/add tags (#18)
    * Tag Managment
    
    ---------
    
    Co-authored-by: Abel Bordonado <abelbordonado@gmail.com>

---
+++ src/modules/tags/tags.router.js
@@ -0,0 +1,32 @@
+import Module from './module.vue'
+import authMiddleware from '@/middlewares/auth'
+import ManageTagsModule from './tags-list'
+
+const moduleRoute = [
+  {
+    path: '/',
+    component: Module,
+    redirect: { name: 'manage-tags' },
+    children: [
+      {
+        path: '/tags',
+        component: Module,
+        redirect: { name: 'manage-tags' },
+        children: [
+          {
+            path: 'list',
+            name: 'manage-tags',
+            component: ManageTagsModule,
+            meta: {
+              middleware: [authMiddleware]
+            }
+          }
+        ]
+      }
+    ]
+  }
+]
+
+export default (router) => {
+  router.addRoutes(moduleRoute)
+}
